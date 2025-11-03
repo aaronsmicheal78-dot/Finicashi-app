6076c63f1e47b7162d2a1fd01676f858eb6baf0e
MWESIGYE ALONE <aaronsmicheal78@gmail.com>
2025-11-03 21:41:29 +0300

    Add requirements for PostgreSQL Render

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Load .env only in local development
-load_dotenv()
+#load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 instance_dir = os.path.join(basedir, "instance")
